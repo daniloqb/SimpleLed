3233ac2628e69ba05a5d4278ff34192cbe733cae
daniloqb <daniloqb@gmail.com>
2017-06-23 12:57:33 -0300

    Adding on() off()

--- SimpleLed.cpp
+++ SimpleLed.cpp
@@ -1,114 +1,74 @@
 #include "Arduino.h"
-#include "MoodLamp.h"
+#include "SimpleLed.h"
 
 
 
-MoodLamp::MoodLamp(byte pin_red, byte pin_green, byte pin_blue){
+SimpleLed::SimpleLed(byte pin_led){
 
-  _pin_red    = pin_red;
-  _pin_green  = pin_green;
-  _pin_blue   = pin_blue;
-  _color      = DEFAULT_COLOR;
+  _pin_led    = pin_led;
   _brightness = DEFAULT_BRIGHTNESS;
   _last_brightness = _brightness;
 }
 
 
-void MoodLamp::begin(){
+void SimpleLed::begin(){
 
-  pinMode(_pin_red,  OUTPUT);
-  pinMode(_pin_green,OUTPUT);
-  pinMode(_pin_blue, OUTPUT);
+  pinMode(_pin_led,  OUTPUT);
+  analogWrite(_pin_led,  0);
 
-  digitalWrite(_pin_red,  LOW);
-  digitalWrite(_pin_green,LOW);
-  digitalWrite(_pin_blue, LOW);
-
-  _wheel_time = 0;
   _fade_time = 0;
   _fade_offset = 0;
   _fade_min = 10;
 
+  _val_led = 0;
 
-  wheel(0);
+
   fade(0);
   fade_speed(DEFAULT_FADE_SPEED);
-  wheel_speed(DEFAULT_WHEEL_SPEED);
-
-
 }
 
 
-int MoodLamp::color(){
-  return _color;
-}
-
-int MoodLamp::color(int color){
-
-  if ((color < 0) || (color > 360)){
-    return -1;
-  }
-
-  _color = color;
-  return _color;
-}
-
-
-int MoodLamp::brightness(){
+int SimpleLed::brightness(){
   return _brightness;
 }
 
-int MoodLamp::brightness(int brightness){
+int SimpleLed::brightness(int brightness){
 
   if ((brightness < 0) || (brightness > 100)){
     return -1;
   }
   _brightness = brightness;
+
+  _val_led = _brightness;
+  _val_led = map(_val_led,0,100,0,255);
+
+  #if defined(PWMRANGE)
+    _val_led =   map(_val_led,  0,255,0,PWMRANGE);
+  #endif
+
+ analogWrite(_pin_led,_val_led);
+
+
+
   return _brightness;
 }
 
-int MoodLamp::last_brightness(){
+int SimpleLed::last_brightness(){
   return _last_brightness;
 }
 
-int MoodLamp::last_brightness(int brightness){
+int SimpleLed::last_brightness(int brightness){
 
   _last_brightness = brightness;
   return _last_brightness;
 }
 
-boolean MoodLamp::wheel(){
-  return _wheel_state;
-}
-boolean MoodLamp::wheel(int state){
 
-  if((state < 0) || (state > 1)){
-    return false;
-  }
-
-  _wheel_state = state;
-  return true;
-}
-
-int MoodLamp::wheel_speed(int speed){
-
-  if((speed < MIN_WHEEL_SPEED) or (speed > MAX_WHEEL_SPEED)){
-    return -1;
-  }
-
-  _wheel_speed = speed;
-  return _wheel_speed;
-}
-
-int MoodLamp::wheel_speed(){
-  return _wheel_speed;
-}
-
-boolean MoodLamp::fade(){
+boolean SimpleLed::fade(){
   return _fade_state;
 }
 
-boolean MoodLamp::fade(int state){
+boolean SimpleLed::fade(int state){
 
   if((state < 0) || (state > 1)){
     return false;
@@ -120,7 +80,7 @@ boolean MoodLamp::fade(int state){
 
 
 
-int MoodLamp::fade_speed(int speed){
+int SimpleLed::fade_speed(int speed){
 
   if((speed < MIN_FADE_SPEED) || (speed > MAX_FADE_SPEED)){
     return -1;
@@ -130,114 +90,32 @@ int MoodLamp::fade_speed(int speed){
   return _fade_speed;
 }
 
-int MoodLamp::fade_speed(){
+int SimpleLed::fade_speed(){
   return _fade_speed;
 }
 
 
 
-
-
-void MoodLamp::update_color(){
-
- int color_ = map(color(), 0, 360, 0, 255);
- int brightness_ = map(brightness(), 0, 100, 0, 255);
-
-  // Caso especial para 255, sera usado para a cor branca, e por isso eh necessario tratar o brightness diferente
-  // inverto o brightness e subtraio esse valor das cores RGB do sistema.
-  if (color_ == 255) {
-    int aux_brightness = map(brightness_, 0, 255, 255, 0);
-
-    _red   = 255 - aux_brightness;
-    _green = 255 - aux_brightness;
-    _blue  = 255 - aux_brightness;
-
-  } else {
-    hueToRGB(color_, brightness_); // caso nao for branca chama a funcao hueToRGB para atualizar os valores das variaveis
-
-  }
-
-  // Escrevo os valores nos pinos.
-  analogWrite(_pin_red,   _red);
-  analogWrite(_pin_green, _green);
-  analogWrite(_pin_blue,  _blue);
-
-}
-
-
-
-void MoodLamp::update_color(int color_, int brightness_){
-
-
-  color(color_);
-  brightness(brightness_);
-
-  color_ = map(color(), 0, 360, 0, 255);
-  brightness_ = map(brightness(), 0, 100, 0, 255);
-
-  // Caso especial para 255, sera usado para a cor branca, e por isso eh necessario tratar o brightness diferente
-  // inverto o brightness e subtraio esse valor das cores RGB do sistema.
-  if (color_ == 255) {
-    int aux_brightness = map(brightness_, 0, 255, 255, 0);
-
-    _red   = 255 - aux_brightness;
-    _green = 255 - aux_brightness;
-    _blue  = 255 - aux_brightness;
-
-  } else {
-    hueToRGB(color_, brightness_); // caso nao for branca chama a funcao hueToRGB para atualizar os valores das variaveis
-
-  }
-
-  // Escrevo os valores nos pinos.
-  analogWrite(_pin_red,   _red);
-  analogWrite(_pin_green, _green);
-  analogWrite(_pin_blue,  _blue);
-
-}
-
-
-void MoodLamp::update(){
-
-  if(wheel() == 1){
-    color_wheel();
-  }
+void SimpleLed::update(){
 
   if(fade() == 1){
-    color_fade();
+    led_fade();
   }
-  update_color();
+
+  Serial.print("Val: ");
+  Serial.println(_val_led);
 }
 
 
 
 
-void MoodLamp::color_wheel() {
-
-  int color_;
-
-  if (millis() - _wheel_time > (unsigned long)wheel_speed()) {
-
-    color_ = color();
-    color_++;
-    color_ = color_ % 359;
-
-    color(color_);
-    _wheel_time  = millis();
-
-    //update_color();
-  }
-
-}
-
-
-void MoodLamp::color_fade() {
+void SimpleLed::led_fade() {
 
  int brightness_;
 
 if (millis() - _fade_time > (unsigned long)fade_speed()) {
- 
- 
+
+
 
  brightness_ = -abs(_fade_offset-100)+100;
 
@@ -255,68 +133,16 @@ if (millis() - _fade_time > (unsigned long)fade_speed()) {
 
 }
 
-void MoodLamp::hueToRGB(int hue, int brightness) {
 
-  unsigned int scaleHue = (hue * 6);
-  unsigned int segment = scaleHue / 256;
-  unsigned int segmentOffset = scaleHue - (segment * 256);
+void SimpleLed::on(){
+ fade(0);
+ if(_last_brightness > 0)
+   brightness(_last_brightness);
+else
+   brightness(DEFAULT_BRIGHTNESS);
+}
 
-  unsigned int complement = 0;
-  unsigned int prev = (brightness * (255 - segmentOffset)) / 256;
-  unsigned int next = (brightness * segmentOffset) / 256;
-
-  if (_invert) {
-    brightness = 255 - brightness;
-    complement = 255;
-    prev = 255 - prev;
-    next = 255 - next;
-  }
-
-
-  switch (segment) {
-    case 0: //red
-      _red = brightness;
-      _green = next;
-      _blue = complement;
-      break;
-    case 1: // yellow
-      _red = prev;
-       _green= brightness;
-      _blue = complement;
-      break;
-    case 2: // green
-    _red = complement;
-      _green = brightness;
-      _blue = next;
-      break;
-    case 3: // cyan
-     _red= complement;
-      _green = prev;
-      _blue = brightness;
-      break;
-    case 4: // blue;
-      _red = next;
-      _green = complement;
-      _blue = brightness;
-      break;
-    case 5: // magenta
-      _red= brightness;
-      _green = complement;
-      _blue = prev;
-      break;
-  }
-
-
-  #if defined(PWMRANGE)
-
-  _red =   map(_red,  0,255,0,PWMRANGE);
-  _green = map(_green,0,255,0,PWMRANGE);
-  _blue =  map(_blue, 0,255,0,PWMRANGE);
-
-
-  #endif
-
-
-
-
+void SimpleLed::off(){
+  fade(0);
+  brightness(0);
 }
--- SimpleLed.h
+++ SimpleLed.h
@@ -1,80 +1,44 @@
-#ifndef MOODLAMP_H
-#define MOODLAMP_H
+#ifndef SIMPLELED_H
+#define SIMPLELED_H
 
 #endif
 
 #include "Arduino.h"
 
 /*
- * MoodLamp -
+ * SimpleLed -
  *
- * This device controls an RGB LED, for mutiple purposes.
+ * This device controls a Simple Led Lamp for mutiple purposes.
  *
- * The device's color follows an circular pattern, it means, that the colors
- * vary form 0 to 360.
- *
+
  *  The available functions are:
- *
- *  - choose color by value : 0 - 360.   0 - red.  360 - white
+ *  - on;
+ *  - off;
+ *  - blink;
  *  - choose brightness: 0 - 100.  0 - led off
  *
- *  - color transition effect. wheel.
- *  - it is possible to change the transition speed: from 0 to 100
- *
+
  *  - fade effect
  *  - it is possible to chance the fade speed from  0 to 10000
  *
- *  This device controls 3 PWM ports that change the colors
+ *  This device controls 1 PWM port
  *
 */
-  /*
-
-   COLORS CONSTANT
-*/
-  const byte  COLOR_RED      = 0;
-  const byte  COLOR_ORANGE   = 30;
-  const byte  COLOR_YELLOW   = 60;
-  const byte  COLOR_LIME     = 90;
-  const byte  COLOR_GREEN    = 120;
-  const byte  COLOR_AQUA     = 150;
-  const byte  COLOR_CYAN     = 180;
-  const byte  COLOR_OCEAN    = 210;
-  const byte  COLOR_BLUE     = 240;
-  const byte  COLOR_PURPLE   = 255;
-  const short  COLOR_VIOLET  = 270;
-  const short  COLOR_MAGENTA = 300;
-  const short  COLOR_PINK    = 330;
-  const short  COLOR_WHITE   = 360;
-
-
-  const int MIN_WHEEL_SPEED = 0;
-  const int MAX_WHEEL_SPEED = 1000;
-  const int DEFAULT_WHEEL_SPEED = 100;
 
   const int MIN_FADE_SPEED = 0;
   const int MAX_FADE_SPEED = 10000;
   const int DEFAULT_FADE_SPEED = 200;
 
-  const int DEFAULT_COLOR = 360;
   const int DEFAULT_BRIGHTNESS = 100;
 
 
 
-class MoodLamp{
+class SimpleLed{
 
   private:
-    int _red;
-    int _green;
-    int _blue;
-    int  _color;
-    boolean _invert = false;
+
     int _brightness;
     int _last_brightness;
-    int _last_color;
-
-    byte _wheel_state;
-    int  _wheel_speed;
-    long _wheel_time;
 
     byte _fade_state;
     byte _fade_speed;
@@ -83,37 +47,26 @@ class MoodLamp{
     int _fade_min;
 
 
-    byte _pin_red;
-    byte _pin_green;
-    byte _pin_blue;
+    byte _pin_led;
+    int  _val_led;
 
-    void hueToRGB(int hue, int brightness);
-    void color_wheel();
-    void color_fade();
+    void led_fade();
 
   public:
-    MoodLamp(byte pin_red, byte pin_green, byte pin_blue);
+    SimpleLed(byte pin_led);
     void begin();
 
-    int color();
-    int color(int color_);
     int brightness();
     int brightness(int brightness_);
     int last_brightness();
     int last_brightness(int brightness_);
-    boolean wheel();
-    boolean wheel(int state);
-    int wheel_speed();
-    int wheel_speed(int speed);
     boolean fade();
     boolean fade(int state);
     int fade_speed();
     int fade_speed(int speed);
 
-    void update_color();
-    void update_color(int color_, int brightness_);
-
-
+    void on();
+    void off();
     void update();
 
 };
