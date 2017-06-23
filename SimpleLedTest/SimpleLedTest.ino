96bfc52dfa2bfd113961c061a1ebced252900e02
daniloqb <daniloqb@gmail.com>
2017-06-23 13:01:28 -0300

    Adding a Test

---
+++ SimpleLedTest/SimpleLedTest.ino
@@ -0,0 +1,25 @@
+#include <SimpleLed.h>
+
+
+
+SimpleLed led(9);
+
+void setup() {
+  // put your setup code here, to run once:
+  led.begin();
+  led.fade(1);
+  led.fade_speed(5);
+  
+
+  
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+led.update();
+
+  
+
+}
